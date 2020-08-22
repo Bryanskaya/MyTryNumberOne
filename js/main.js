8ab798a301aa78223e5a29460db5d824cd53bc3a
Bryanskaya <54107546+Bryanskaya@users.noreply.github.com>
2020-08-22 13:08:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+     el: '.project-pagination',
+     bulletClass: 'projects-bullet',
+     bulletActiveClass: 'projects-bullet-active',
+     clickable: true
+	},
+});
